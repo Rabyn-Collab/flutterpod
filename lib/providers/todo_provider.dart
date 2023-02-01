5d31f0b87e54cf8223eac1f47249b89695186784
Rabyn-Collab <babynshrestha64@gmail.com>
2023-02-01 17:10:31 +0545

    changes

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -10,6 +10,25 @@ final todoProvider = StateNotifierProvider<TodoProvider, List<Todo>>((ref) => To
 class TodoProvider extends StateNotifier<List<Todo>>{
   TodoProvider(super.state);
 
+  void add (Todo todo){
+  state = [...state, todo];
+  }
+
+  void remove(Todo todo){
+    state.remove(todo);
+    state = [...state];
+  }
+
+  void update(Todo updatedTodo, int index){
+    state[index] = updatedTodo;
+    state = [...state];
+    // state = [
+    //   for(final todo in state)
+    //     if(updatedTodo == todo) updatedTodo else todo
+    // ];
+
+  }
+
 
 
 }
