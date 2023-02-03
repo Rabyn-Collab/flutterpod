fe9cd3b9b029be474af6724e51a2ae7f101b108c
Rabyn-Collab <babynshrestha64@gmail.com>
2023-02-03 16:37:41 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutterpod/view/home_page.dart';
 import 'package:get/get.dart';
 
+final dio = Dio();
+
+Future getData()async{
+ return dio.get('https://api.themoviedb.org/3/movie/popular', queryParameters: {
+
+ });
+}
+
+void main() async{
+
+  try{
+    final response = await getData();
+    print(response.data);
+  }on DioError catch (err){
+     print(err.response);
+  }
 
 
+   // getData().then((value){
+   //   print(value);
+   // }).catchError((err) => print(err));
 
-void main(){
 runApp(ProviderScope(child: Home()));
 }
 
